ea35d69479f04090184233de8e6b917fa4af0f76
digitalm2016 <98210819+digitalm2016@users.noreply.github.com>
2024-07-22 15:47:01 +0530

    Update start_container.sh

--- scripts1/start_container.sh
+++ scripts1/start_container.sh
@@ -1,3 +1,3 @@
 set -e
-docker pull digitalm2016/nodejs-app-mss
-docker run -d -p 9981:9981 digitalm2016/nodejs-app-mss
+sudo docker pull digitalm2016/nodejs-app-mss
+sudo docker run -d -p 9981:9981 digitalm2016/nodejs-app-mss
